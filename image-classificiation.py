8dfb380a6d26330fba5db29a189c7d843965ec3a
Utku Sen <144535374+utkyfact@users.noreply.github.com>
2024-10-03 18:28:27 +0300

    Update and rename image_classificiation.py to image-classificiation.py

--- image-classificiation.py
+++ image-classificiation.py
@@ -2,8 +2,8 @@
 # # Image Classification using Transfer Learning
 
 import numpy as np
-from PIL import Image # Python Imaging Library - For operations like: Image open, resize image, etc..
-from IPython.display import Image as show_image  # For displaying our test images to you
+from PIL import Image
+from IPython.display import Image as show_image
 from tensorflow.keras.applications.inception_resnet_v2 import InceptionResNetV2
 from tensorflow.keras.preprocessing import image
 from tensorflow.keras.applications.inception_resnet_v2 import preprocess_input, decode_predictions
